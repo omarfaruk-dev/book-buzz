e891f9216b3edb8316db1c1c423c1a2705ada572
Omar Faruk <omarfaruk.dev@gmail.com>
2025-04-16 22:10:56 +0600

    initial file setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+    createBrowserRouter,
+  } from "react-router";
+  
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello World</div>,
+    },
+  
+  ]);
